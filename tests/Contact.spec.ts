b44a25438779ff42d79db70858180be3a4154ba7
bengroneman <bluegroneman10@gmail.com>
2022-12-11 19:01:34 -0700

    test correction

--- tests/Contact.spec.ts
+++ tests/Contact.spec.ts
@@ -14,7 +14,7 @@ test('able to fill out form', async ({page}) => {
     await page.locator("input[name='full-name']").type("Ben Groneman")
     await page.locator("input[name='email']").type("clio@clippy.org")
     await page.locator("input[name='phone']").type("1-111-111-1111")
-    await page.locator("input[name='message']").type("A placeholder text as this is merely a test of the system")
+    await page.locator("textarea[name='message']").type("A placeholder text as this is merely a test of the system")
 
     await page.locator('#submit').click();
 });
