74db01e7bd3d6dc89fd40a7a9100ede3ecb07c69
Blue Groneman <hello@bluedojo.dev>
2022-12-26 09:00:45 -0700

    fixed mobile menu to work w/astro

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
     import { navigation } from "../data/navigation";
+    let mobileMenu = false;
+    let toggleMobileMenu = () => {
+        console.log(mobileMenu)
+        mobileMenu = !mobileMenu;
+    }
 </script>
 <nav class="bg-gray-800">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="flex h-16 items-center justify-between">
             <div class="flex items-center">
                 <div class="flex-shrink-0">
-                    <img class="block h-8 w-auto lg:hidden" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
-                    <img class="hidden h-8 w-auto lg:block" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
+                    <img class="block h-8 w-auto lg:hidden" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Blue Engineering LLC">
+                    <img class="hidden h-8 w-auto lg:block" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Blue Engineering LLC">
                 </div>
                 <div class="hidden sm:ml-6 sm:block">
                     <div class="flex space-x-4">
@@ -16,7 +21,7 @@
                             <a
                                 href="{item.url}"
                                 id="{item.title}"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                                class="nav-link"
                             >
                                 {item.title}
                             </a>
@@ -24,43 +29,38 @@
                     </div>
                 </div>
             </div>
-            <div class="-mr-2 flex sm:hidden">
+            <div class="-mr-2 flex sm:hidden" on:click={toggleMobileMenu}>
                 <!-- Mobile menu button -->
-                <button type="button" class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                <button
+                    type="button"
+                    class="mobile-menu"
+                    aria-controls="mobile-menu"
+                >
                     <span class="sr-only">Open main menu</span>
-                    <!--
-                      Icon when menu is closed.
-
-                      Heroicon name: outline/bars-3
-
-                      Menu open: "hidden", Menu closed: "block"
-                    -->
-                    <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                    </svg>
-                    <!--
-                      Icon when menu is open.
-
-                      Heroicon name: outline/x-mark
-
-                      Menu open: "block", Menu closed: "hidden"
-                    -->
-                    <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
+                    {#if !mobileMenu}
+                        <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                        </svg>
+                    {:else}
+                        <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    {/if}
                 </button>
+
             </div>
         </div>
     </div>
 
     <!-- Mobile menu, show/hide based on menu state. -->
+    {#if mobileMenu}
     <div class="sm:hidden" id="mobile-menu">
         <div class="space-y-1 px-2 pt-2 pb-3">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
             {#each navigation as item}
                 <a
                     href="{item.url}"
-                    class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                    class="nav-link"
                 >
                     {item.title}
                 </a>
@@ -71,4 +71,5 @@
             </div>
         </div>
     </div>
+    {/if}
 </nav>
