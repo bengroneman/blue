bacbcd53554f9e2604cca0c568fc004b795d6fe3
Blue Groneman <hello@bluedojo.dev>
2023-01-23 22:43:49 -0700

    resume page and strapii config

---
+++ src/components/ContentWrapper.svelte
@@ -0,0 +1,7 @@
+<div class="relative overflow-hidden bg-white py-16">
+  <div class="hidden lg:absolute lg:inset-y-0 lg:block lg:h-full lg:w-full lg:[overflow-anchor:none]">
+    <div class="relative mx-auto h-full max-w-prose text-lg" aria-hidden="true">
+    <slot></slot>
+    </div>
+  </div>
+</div>
